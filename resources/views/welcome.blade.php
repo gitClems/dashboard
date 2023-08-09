1dbd0c6f5b14062e65bf71df7269dd8edf66b1f5
BAMOGO Clément <clement_bamogo@um5.ac.ma>
2023-08-09 10:05:26 +0200

    m

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Page 1</title>
+</head>
+
+<body>
+    <h1>
+        The first page we could see after lunching the app
+    </h1>
+</body>
+
+</html>
